92228b09b34e02d15faad54ec983e5afd2a0ec52
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-09-30 15:30:04 -0700

    Print more rows in the table inspector

--- lua/gcompute/glua/printing/tableprinter.lua
+++ lua/gcompute/glua/printing/tableprinter.lua
@@ -88,7 +88,7 @@ function self:PrintMultiline (printer, coloredTextSink, obj, printingOptions, al
 			end
 		)
 		
-		local maxKeyIndex = math.min (160, #sortedKeys)
+		local maxKeyIndex = math.min (500, #sortedKeys)
 		local maxKeyLength = 0
 		local maxBracketedKeyLength = 0
 		
