e4eceaafe95c025b7af48046e4d59f9e53df1607
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-03-15 15:05:19 -0700

    Simple check

--- lua/autorun/gcompute.lua
+++ lua/autorun/gcompute.lua
@@ -1,5 +1,7 @@
 if SERVER or
    file.Exists ("gcompute/gcompute.lua", "LUA") or
    file.Exists ("gcompute/gcompute.lua", "LCL") and GetConVar ("sv_allowcslua"):GetBool () then
-	include ("gcompute/gcompute.lua")
+   include ("gcompute/gcompute.lua")
+else
+   CreateClientConVar("is_gcompute_user", 0, true, true)
 end
